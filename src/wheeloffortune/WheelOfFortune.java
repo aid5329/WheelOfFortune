570bdb70a8cb02d7be3650cb91eb3a5e7be90f37
aminatadiarra <aminatadiarra@10.0.0.164>
2016-09-16 22:05:02 -0700

    Wheel of Fortune Sprint1

--- src/wheeloffortune/WheelOfFortune.java
+++ src/wheeloffortune/WheelOfFortune.java
@@ -7,13 +7,44 @@
 */
 package wheeloffortune;
 
+import java.util.Scanner;
+
 public class WheelOfFortune {
 
   /**
    * @param args the command line arguments
    */
   public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+        String userInput = " ";
+
+        System.out.println("Please enter Spin The Wheel, Buy a Vowel, Solve the Puzzle, or Quit The Game");
+        userInput = keyboard.nextLine();
+
+        System.out.println("You selected: " + userInput);
+
+        System.out.println("Enter a letter");
+        userInput = keyboard.nextLine();
+
+        System.out.println("You Entered: " + userInput);
+
+        System.out.print("Enter a letter: ");
+        String s = " ";
+
+        char ch = Character.toUpperCase(s.charAt(0));
+        if (ch > 'Z' || ch < 'A') {
+            System.out.println(s + " is an invalid input");
+          System.exit(1);
+        }
+        
+        System.out.println("If you would like to test, enter TEST");
+        userInput = keyboard.nextLine();
     
+        if (ch > 'Z' || ch < 'A') {
+            System.out.println(s + " is an invalid input");
+            System.exit(1);
+        }
+    }
   }
   
-}
+
