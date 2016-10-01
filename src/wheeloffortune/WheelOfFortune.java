684d5a4ae1e770f97f9b5b4c282eeeb63188811c
aminatadiarra <aminatadiarra@10.0.0.164>
2016-09-30 23:37:22 -0700

    Wheel of Fortune Sprint3

--- src/wheeloffortune/WheelOfFortune.java
+++ src/wheeloffortune/WheelOfFortune.java
@@ -2,7 +2,9 @@ package wheeloffortune;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -10,171 +12,260 @@ import java.util.Scanner;
 * CMPSC 261, Section 1
 * Fall 2016
 * Instructor: Phil O'Connell
-* Student: Phil O'Connell 
-* ID: pxo4
+* Student: Aminata Diarra
+* ID: aid5329
  */
 public class WheelOfFortune {
+    // To read from the keyboard
+  private static final Scanner _keyboard = new Scanner(System.in);
 
-    private static final Scanner _keyboard = new Scanner(System.in);
+  // Used to get random values for puzzle and wheel
+  private static final Random _random = new Random();
 
-    private static final List<String> _menuChoices = Arrays.asList(
-            "==============================="
-            + "=       Wheel of Fortune      ="
-            + "===============================",
-            "1.  Spin the wheel",
-            "2.  Buy a vowel",
-            "3.  Solve the puzzle",
-            "4.  Quit the game",
-            "9.  Test letter input"
-    );
+  // True if we want to show all letters
+  private static boolean revealLetters = false;
 
-    private static final int _quitChoiceNumber = 4;
+  /*
+  * These are the wedges that are part of the wheel.
+  * There are 24.  Some values can appear more than once
+  */
+  private static final List<String> _wedges = Arrays.asList(
+      /* 01 */"$5000",
+      /* 02 */ "$600",
+      /* 03 */ "$500",
+      /* 04 */ "$300",
+      /* 05 */ "$500",
+      /* 06 */ "$800",
+      /* 07 */ "$550",
+      /* 08 */ "$400",
+      /* 09 */ "$300",
+      /* 10 */ "$900",
+      /* 11 */ "$500",
+      /* 12 */ "$300",
+      /* 13 */ "$900",
+      /* 14 */ "BANKRUPT",
+      /* 15 */ "$600",
+      /* 16 */ "$400",
+      /* 17 */ "$300",
+      /* 18 */ "LOSE A TURN",
+      /* 19 */ "$800",
+      /* 20 */ "$350",
+      /* 21 */ "$450",
+      /* 22 */ "$700",
+      /* 23 */ "$300",
+      /* 24 */ "$600"
+  );
 
-    private static boolean isValidMenuChoice(int choice) {
-        if (choice < 1 || choice > _menuChoices.size()) {
-            return false;
+  /*
+  * The number of wedges will not change throughout the game
+  * We can cache the value so we're not calling .size() over and over
+  */
+  private static final int _wedgeCount = _wedges.size();
+
+  private static String chooseRandomWedgeValue() {
+    // Choose a random index
+    int randomWedgeIndex = _random.nextInt(_wedgeCount);
+
+    // Return the corresponding wedge
+    return _wedges.get(randomWedgeIndex);
+  }
+
+  // The menu choices
+  private static final List<String> _menuChoices = Arrays.asList(
+      "1. Spin the wheel",
+      "2. Buy a vowel",
+      "3. Solve the puzzle",
+      "4. Quit the game",
+      "", // 5 possibly used in the future
+      "", // 6 possibly used in the future
+      "" // 7 possibly used in the future
+
+  );
+  private static final int _quitChoiceNumber = 4;
+
+  // The different puzzles to choose from
+  private static final List<String> _puzzles = Arrays.asList(
+      "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG",
+      "PENN STATE ABINGTON",
+      "INFORMATION SCIENCES AND TECHNOLOGY",
+      "This election is an insult to our intelligence",
+      "Exercise is the best medecine"
+  );
+
+  /*
+  * The number of puzzles will not change throughout the game
+  * We can cache the value so we're not calling .size() over and over
+  */
+  private static final int _puzzlesCount = _puzzles.size();
+  
+
+  /*
+  * We will store the guessed letters in a hash map.
+  * The "key" will be the character that was guessed
+  * The "value" will be true/false
+  *
+  * Actually, the "value" aspect of this is not relevant.
+  * Just the fact that a letter appears in the map as a key, is enough to imply
+  * it was guessed.
+  */
+  private static Map<Character, Boolean> guessedLetters = new HashMap<>();
+
+  /*
+  * Given a puzzle, return a masked version, with hidden letters
+  */
+  private static String maskPuzzle(String puzzle, boolean revealLetters) {
+    // Use a string builder, since Java strings are immutable
+    StringBuilder maskedPuzzle = new StringBuilder();
+
+    // For each letter in the puzzle
+    for (int i = 0; i < puzzle.length(); i++) {
+      // Current letter
+      char c = puzzle.charAt(i);
+
+      /*
+      * Either we're revealing all letters, or we've already guessed the
+      * letter
+      */
+      boolean isLetterGuessed = revealLetters || guessedLetters.containsKey(c);
+
+      /*
+      * If the letter is not blank (we don't mask blanks), and the letter
+      * has not been guessed, then we will mask it.
+      */
+      if (c != ' ' && !isLetterGuessed){
+        c = '_';
+      }
+      
+      // Put one space after each character (even a space) in the puzzle
+      maskedPuzzle.append(c + " ");
+    }
+
+    // Convert the string builder to a string and return it
+    return maskedPuzzle.toString();
+  }
+
+  // Choose a random puzzle
+  private static String chooseRandomPuzzle() {
+    // Choose a random puzzle index
+    int randomPuzzleIndex = _random.nextInt(_puzzlesCount);
+
+    //Return the corresponding puzzle
+    return _puzzles.get(randomPuzzleIndex);
+  }
+
+  // Determine if the given number choice actually appears on the menu
+  private static boolean isValidMenuChoice(int choice) {
+    if ((choice < 1) || (choice > _menuChoices.size())) {
+      return false;
+    }
+
+    // Subtrace 1 because arrays/lists are zero-based
+    int index = choice - 1;
+    String menuText = _menuChoices.get(index);
+
+    return !menuText.equals("");
+  }
+
+  // Input a letter from the keyboard
+  private static char inputLetter() {
+    char letter = ' ';
+    boolean finished = false;
+
+    while (!finished) {
+      System.out.print("Enter a letter: ");
+
+      String line = _keyboard.nextLine();
+      if (line.length() != 1) {
+        System.out.println("Enter just one letter");
+      } else {
+        // Convert letter to upper case
+        letter = Character.toUpperCase(line.charAt(0));
+        if (!Character.isLetter(letter)) {
+          System.out.println("That is not a letter");
+        } else {
+          // Will exit the loop
+          finished = true;
         }
-
-        int index = choice - 1;
-        String menuText = _menuChoices.get(index);
-
-        return !menuText.equals("");
+      }
     }
 
-    private static char inputLetter() {
-        char letter = ' ';
-        boolean finished = false;
+    return letter;
+  }
 
-        while (!finished) {
-            System.out.print("Enter a letter: ");
+  // Display the game menu, and handle the choices made
+  private static void gameMenu() {
+    // Choice from the menu
+    int choice = 0;
 
-            String line = _keyboard.nextLine();
-            if (line.length() != 1) {
-                System.out.println("Enter just one letter");
-            } else {
-                letter = line.charAt(0);
-                if (!Character.isLetter(letter)) {
-                    System.out.println("That is not a letter");
-                } else {
-                    finished = true;
-                }
-            }
+    // Line entered from keyboard
+    String line = "";
+
+    // True when user wants to quit
+    boolean quit = false;
+
+    // Choose one of the puzzles at random
+    String puzzle = chooseRandomPuzzle();
+
+    // Repeat the menu until the user chooses to quit
+    while (!quit) {
+      System.out.println("                      ======================");
+      System.out.println("                      =  Wheel Of Fortune  =");
+      System.out.println("                      ======================");
+      System.out.println("                                            ");
+
+      System.out.println(maskPuzzle(puzzle, revealLetters));
+      System.out.println();
+
+      // Loop over the menu choices, and display each one
+      for (String menuChoice : _menuChoices) {
+        // Skip blank place-holder choices
+        if (!menuChoice.equals("")) {
+          System.out.println(menuChoice);
         }
+      }
+      System.out.print("Enter choice: ");
+      line = _keyboard.nextLine();
+      try {
+        // If the input was not an integer, then that error will be caught
+        choice = Integer.parseInt(line);
+      } catch (NumberFormatException nfe) {
+        // Error message, then go to the top of the loop
+        System.out.println("Invalid input");
+        continue;
+      }
 
-        return letter;
+      // If not valid, then go back to the top of the loop
+      if (!isValidMenuChoice(choice)) {
+        System.out.println("Not a menu choice");
+        continue;
+      }
+
+      System.out.println("You chose: " + _menuChoices.get(choice - 1));
+      switch (choice) {
+        case _quitChoiceNumber:
+          // This will allow us to leave the menu loop
+          quit = true;
+          break;
+
+        case 1: // Spin the wheel
+          System.out.println("You landed on: " + chooseRandomWedgeValue());
+          char letter = inputLetter();
+          System.out.println("Your letter is: " + letter);
+          guessedLetters.put(letter, true);
+          break;
+
+        
+      }
     }
+  }
 
-    private static void gameMenu() {
-        int choice = 0;
-        String line = "";
-        boolean quit = false;
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    gameMenu();
+  }
 
-        while (!quit) {
-            System.out.println("\n===============================================================================");
-            for (String menuChoice : _menuChoices) {
-                if (!menuChoice.equals("")) {
-                    System.out.println(menuChoice);
-                }
-            }
-            System.out.print("Enter choice: ");
-            line = _keyboard.nextLine();
-            try {
-                choice = Integer.parseInt(line);
-            } catch (NumberFormatException nfe) {
-                System.out.println("Invalid input");
-                continue;
-            }
-
-            if (!isValidMenuChoice(choice)) {
-                System.out.println("Not a menu choice");
-                continue;
-            }
-
-            System.out.println("You chose: " + _menuChoices.get(choice - 1));
-            switch (choice) {
-                case _quitChoiceNumber:
-                    quit = true;
-                    break;
-                case 9:
-                    System.out.println("Your letter is: " + inputLetter());
-                    break;
-            }
-        }
-
-        // As a user i want to guess the letter
-        String secretPhrase = "THE QUICK BROWN FOX JUMPS COVER THE LAZE DOG";
-        String guesses = " "; //the user's guess
-        Scanner keyboard = new Scanner(System.in);
-
-        boolean notDone = true;
-        while (notDone) {
-            notDone = false;
-            for (char secretletter : secretPhrase.toCharArray()) {
-                if (guesses.indexOf(secretletter) == -1) {
-                    System.out.println("_");
-                    notDone = true;
-                } else {
-                    System.out.println(secretletter);
-                }
-            }
-            System.out.print("\nEnter your lettr: ");
-            String letter = keyboard.next();
-            guesses += letter;
-        }
-        System.out.println("You guesses right!");
-
-    }
-
-    /*
-    if(player_menu == 1) {
-    player_menu++;
-    spin = rand() % 25;
-    cout<<endl<<"The wheel landed on: $" <<wheel[spin] << " . " << endls;
-
-     */
-    public static void main(String[] args) {
-        gameMenu();
-
-    }
-
-    private static final int _spinChoiceNumber = 1;
-
-    {
-
-        Integer[] wheel = new Integer[22];
-
-        wheel[0] = 300;
-        wheel[1] = 300;
-        wheel[2] = 300;
-        wheel[3] = 300;
-        wheel[4] = 300;
-        wheel[5] = 350;
-        wheel[6] = 400;
-        wheel[7] = 400;
-        wheel[8] = 450;
-        wheel[9] = 500;
-        wheel[10] = 500;
-        wheel[11] = 500;
-        wheel[12] = 550;
-        wheel[13] = 600;
-        wheel[14] = 600;
-        wheel[15] = 600;
-        wheel[16] = 700;
-        wheel[17] = 800;
-        wheel[18] = 800;
-        wheel[19] = 900;
-        wheel[20] = 900;
-        wheel[21] = 5000;
-
-        List<String> letterwheel = new ArrayList<>();
-        letterwheel.add("You lost turn");
-        letterwheel.add("Bankrupt!");
-
-    }
-
-    public static int getRandom(int[] array) {
-        Integer wheel = new Random().nextInt(array.length);
-        return array[wheel];
-    }
+    
 }
 
